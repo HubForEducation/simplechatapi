b152ddc25aa75f1235111efa259cb39774d69223
Anonymous <Anonymous@Anonymous.Com>
2019-04-03 22:30:49 +0300

    WOW! ITS WORK!

--- SimpleChatApi/SimpleChatApi/Controllers/MessagesController.cs
+++ SimpleChatApi/SimpleChatApi/Controllers/MessagesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
 namespace SimpleChatApi.Controllers
@@ -12,23 +13,12 @@ namespace SimpleChatApi.Controllers
     public class MessagesContoller : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-        [HttpGet("getall")]
-        public ActionResult<string> GetAll()
+        public SimpleChatApi.Messages[] Get()
         {
             using (MessagesContext db = new MessagesContext())
             {
-                return null;
+                return db.messages.ToArray();
             }
-
         }
         [HttpPost]
         public void Post(int id, string autor, string body)
