bce8770fdefa3d3a5e7a451b9636708d12a9afd3
Anonymous <Anonymous@Anonymous.Com>
2019-04-03 23:28:23 +0300

    Add GET and POST.

--- SimpleChatApi/SimpleChatApi/Controllers/MessagesController.cs
+++ SimpleChatApi/SimpleChatApi/Controllers/MessagesController.cs
@@ -13,24 +13,51 @@ namespace SimpleChatApi.Controllers
     public class MessagesContoller : ControllerBase
     {
         [HttpGet]
-        public SimpleChatApi.Messages[] Get()
+        public SimpleChatApi.Messages[] Get(int id, string autor, string body)
         {
             using (MessagesContext db = new MessagesContext())
             {
-                return db.messages.ToArray();
+                var returnvalue = db.messages.ToArray();
+
+                if (id != 0)
+                {
+                    returnvalue = db.messages.Where(i => i.id == id).ToArray();
+                }
+
+                if (autor != null)
+                {
+                    returnvalue = db.messages.Where(i => i.autor == autor).ToArray();
+                }
+
+                if (body != null)
+                {
+                    returnvalue = db.messages.Where(i => i.body == body).ToArray();
+                }
+
+                return returnvalue;
             }
         }
-        [HttpPost]
-        public void Post(int id, string autor, string body)
-        {
-            using (MessagesContext db = new MessagesContext())
-            {
-                // создаем два объекта User
-                Messages message = new Messages() { id = id, autor = autor, body = body};
 
-                db.messages.Add(message);
-                db.SaveChanges();
+        [HttpPost]
+        public string Post(int id, string autor, string body)
+        {
+            try
+            {
+                using (MessagesContext db = new MessagesContext())
+                {
+                    Messages message = new Messages() { id = id, autor = autor, body = body };
+
+                    db.messages.Add(message);
+                    db.SaveChanges();
+                }
+
+                return "OK!";
             }
+            catch (DbUpdateException)
+            {
+                return "All fields must be filled.";
+            }
+            
         }
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
